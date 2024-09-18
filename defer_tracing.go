1c640c69e945001ec90864323d2a427980d0cd26
Najem El-Deen Nabhani <42846512+Najem-Nabhani@users.noreply.github.com>
2024-09-18 20:58:40 +0300

    Create defer_tracing.go

---
+++ defer_tracing.go
@@ -0,0 +1,38 @@
+package main
+import "fmt"
+
+func trace(s string) {
+        fmt.Println("entering:", s)
+    } // entering func.
+func untrace(s string) {
+        fmt.Println("leaving:", s)
+    } // leaving func.
+
+func a() {
+    trace("a")
+    defer untrace("a") // untracing via defer
+    fmt.Println("in a")
+}
+
+func b() {
+    trace("b")
+    defer untrace("b") // untracing via defer
+    fmt.Println("in b")
+    a()
+}
+
+func main() {
+    b()
+}
+
+/*
+OUTPUT:
+
+entering: b
+in b
+entering: a
+in a
+leaving: a
+leaving: b
+
+*/
