23030561f9f708f9d5080df621eedda3418e9ea4
Najem El-Deen Nabhani <42846512+Najem-Nabhani@users.noreply.github.com>
2024-11-18 21:36:22 +0300

    Update stack_variabletypes.go

--- stack/stack_variabletypes.go
+++ stack/stack_variabletypes.go
@@ -35,3 +35,21 @@ func (stack *Stack) Pop() (interface{}, error) {
 	*stack = stk[:len(stk)-1] // shrink the stack
 	return top, nil
 }
+
+
+// Usage:
+var st1 mystack.Stack
+
+func main() {
+	st1.Push("Brown")
+	st1.Push(3.14)
+	st1.Push(100)
+	st1.Push([]string{"Java", "C++", "Python", "C#", "Ruby"})
+	for {
+		item, err := st1.Pop()
+		if err != nil {
+			break
+		}
+		fmt.Println(item)
+	}
+}
